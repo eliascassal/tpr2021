dc64f650c58e71e8f25f06b3af285a772a7abf75
eliascassal <eliascassal@gmail.com>
2021-08-10 17:11:24 -0400

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+   return "Bienvenido a la pagina principal";
 });
+
+Route::get('cursos', function(){
+    return "Bienvenido a la pagina de cursos";
+});
+
+Route::get ('cursos/create', function ()     {
+    return "En esta pagina podras crear cursos";
+});
+
+Route::get ('cursos/{curso}/{categoria?}', function ($curso, $categoria = null){
+    
+    if ($categoria){
+    return "Bienvenido al curso : $curso, de la categoria : $categoria";}
+    else{
+    return "Bienvenido al curso : $curso";}
+});
+*/
+Route::get('/', HomeController::class);
+Route::get('cursos', [CursoController::class, 'index']);
+Route::get('cursos/create', [CursoController::class, 'create']);
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
